3d51608cfc2ec320d92e0e06b089d736c8dbe5b7
Jeremiah Samuel <livinalt@gmail.com>
2024-03-28 14:34:50 +0100

    fixes

--- frontend/src/Components/Submission.jsx
+++ frontend/src/Components/Submission.jsx
@@ -33,23 +33,24 @@ function Submission() {
         }
       );
       const resData = await res.json();
-      setImage(`${import.meta.env.VITE_ipfs_base_url + resData.IpfsHash}`);
+
+      setImage(`https://coffee-blank-owl-368.mypinata.cloud/ipfs/${resData.IpfsHash}`);
       console.log(resData);
     } catch (error) {
       console.log(error);
     }
   };
 
+  console.log(image);
   return (
     <>
       <Flex justify={"between"}>
-        <img className="w-10 h-10 rounded-full" src={image} alt="Rounded avatar" />
+        {/* <img className="w-10 h-10 rounded-full" src={image} alt="Rounded avatar" /> */}
       
       <label className="form-label"> Choose File</label>
       <br/>
       <input 
       type="file"   
-      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"    
       onChange={changeHandler} />
       <br />
       <button 
